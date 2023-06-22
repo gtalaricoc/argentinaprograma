58303df5af0b6e8f975fade3b7d3f6d46fb7c720
gtalaricoc <gtalaricoc@gmail.com>
2023-06-22 18:23:35 -0400

    modificacion meses.js

--- practico2/meses.js
+++ practico2/meses.js
@@ -1,4 +1,7 @@
-meses = [
+const readlineSync = require('readline-sync');
+let mes = readlineSync.question("Ingresar un numero entero del 1 al 12\n");
+
+mes = {   
     1:"enero",
     2:"febrero", 
     3:"marzo",
@@ -10,7 +13,21 @@ meses = [
     9:"septiembre",
     10:"octubre",
     11:"noviembre",
-    12:"diciembre"]
+    12:"diciembre"
+}
 
-    const readlineSync = require('readline-sync');
-    let num = readlineSync.question("La cantidad de dias del mes de "+ mes + "es" );
+
+if (mes>=1 && mes<=12){
+    let cantidadDeDias = [];
+}
+
+if (mes == 2) {
+    cantidadDeDias == 28;
+  } else if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
+    cantidadDeDias == 30;
+  } else {
+    cantidadDeDias == 31;
+  }
+
+console.log("La cantidad de dias del mes de " + mes + " es " + cantidadDeDias);
+
