da4857942a69d1ed3bd4602e361a47a1215704df
gtalaricoc <gtalaricoc@gmail.com>
2023-07-08 15:00:10 -0400

    modificacion practico4

--- practico4/piedraPapelTijeras.js
+++ practico4/piedraPapelTijeras.js
@@ -1,11 +1,14 @@
-let jugada = {0: "piedra",1: "papel",2: "tijeras"};
+const readlineSync = require("readline-sync");
+
+
   function obtenerJugadaComputadora(){
-    let numeroAleatorio= Math.ceil(Math.random()*3);
+    let jugada = {0: "piedra",1: "papel",2: "tijeras"};
+    let numeroAleatorio= Math.floor(Math.random()*3);
     return jugada[numeroAleatorio]; 
   }
     
   function obtenerJugadaUsuario() {
-    let jugadaUsuario = ("Ingresa tu jugada: piedra, papel o tijeras");
+    let jugadaUsuario = readlineSync.question("Ingresa tu jugada: piedra, papel o tijeras: ");
     if (!["piedra", "papel", "tijeras"].includes(jugadaUsuario)) {
       console.log("Jugada inválida. Inténtalo nuevamente.");
       return obtenerJugadaUsuario(); 
@@ -14,12 +17,12 @@ let jugada = {0: "piedra",1: "papel",2: "tijeras"};
   }
   
   function determinarGanador(jugadaComputadora, jugadaUsuario) {
-    if (jugadaComputadora == jugadaUsuario) {
+    if (jugadaComputadora === jugadaUsuario) {
       return "Empate";
     } else if (
-      (jugadaComputadora == "piedra" && jugadaUsuario == "tijeras") ||
-      (jugadaComputadora == "tijeras" && jugadaUsuario == "papel") ||
-      (jugadaComputadora == "papel" && jugadaUsuario == "piedra")
+      (jugadaComputadora === "piedra" && jugadaUsuario === "tijeras") ||
+      (jugadaComputadora === "tijeras" && jugadaUsuario === "papel") ||
+      (jugadaComputadora === "papel" && jugadaUsuario === "piedra")
     ) {
       return "Gana la computadora";
     } else {
@@ -32,9 +35,9 @@ let jugada = {0: "piedra",1: "papel",2: "tijeras"};
     let jugadaUsuario = obtenerJugadaUsuario();
     let resultado = determinarGanador(jugadaComputadora, jugadaUsuario);
   
-    console.log("La computadora eligió: ${jugadaComputadora}.");
-    console.log("El usuario eligió: ${jugadaUsuario}.");
-    console.log("El resultado fue: ${resultado}.");
+    console.log("La computadora eligió: "+ jugadaComputadora);
+    console.log("El usuario eligió: "+ jugadaUsuario);
+    console.log("El resultado fue: " + resultado);
   }
   
   jugarPiedraPapelTijeras();
